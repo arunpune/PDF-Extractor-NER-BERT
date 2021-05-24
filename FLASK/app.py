b2f4150ab6ccd92122e3f0f71af3cfa6d10528ee
Atharva Bankar <bankaratharva@gmail.com>
2021-05-24 16:52:36 +0530

    Flask initialise

--- FLASK/app.py
+++ FLASK/app.py
@@ -1,34 +1,63 @@
-from flask import Flask, render_template, request
-import joblib
-import config
+from flask import Flask, render_template, request, send_from_directory, current_app as app
+import os
 import predict
-import dataset
-import numpy as np
-import pandas as pd
+import joblib
+import transformers
 import tensorflow as tf
 import tensorflow as tf
+import pdf_to_img
+import warnings
+warnings.filterwarnings("ignore")
 
 app = Flask(__name__)
 
 meta_data = joblib.load(
-    "/Users/iambankaratharva/CanspiritAI/bert-entity-extraction/FLASK/meta.bin")
+    "/Users/iambankaratharva/CanspiritAI/PDF-Extractor-NER-BERT/FLASK/meta.bin")
 
 model_bert = tf.keras.models.load_model(
-    '/Users/iambankaratharva/CanspiritAI/bert-entity-extraction/FLASK/my_model.h5', compile=False)
+    '/Users/iambankaratharva/CanspiritAI/PDF-Extractor-NER-BERT/FLASK/my_model.h5', compile=False, custom_objects={'TFBertMainLayer': transformers.TFBertModel})
 
+APP_ROOT = os.path.dirname(os.path.abspath(__file__))
 
+# Function for preparing images from pdf
+def prepare_images(pdf_path):
+    # Output dir
+    output_dir = os.path.join(APP_ROOT, 'static/pdf_image/')
+
+    with(Image(filename=pdf_path, resolution=300, width=600)) as source:
+        images = source.sequence
+        pages = len(images)
+        for i in range(pages):
+            Image(images[i]).save(filename=output_dir + str(i) + '.png')
+    print('Prepare Images done')
+
+# App routing
 @app.route('/')
-def upload_file():
-    return render_template('upload.html')
+def main():
+    return render_template('index.html')
 
+@app.route('/upload', methods=['POST'])
+def upload():
+    pdf_target = os.path.join(APP_ROOT, 'static/pdf')
 
-@app.route('/uploader', methods=['GET', 'POST'])
-def upload_file_():
-    if request.method == 'POST':
-        f = request.files['file']
-        sentence = f.read().decode("utf-8")
+    # Preparing directory
+    if not os.path.isdir(pdf_target):
+        os.mkdir(pdf_target)
+
+    # Uploading File
+    for file in request.files.getlist('file'):
+        filename = file.filename
+        destination = "/".join([pdf_target,filename])
+        file.save(destination)
+        print(destination)
+        sentence = pdf_to_img.extractor(destination)
         final_info = predict.get_mapping([sentence], meta_data, model_bert)
-        return final_info
+
+        '''# Creating images
+        if os.path.isfile(destination):
+            prepare_images(destination)'''
+
+    return final_info
 
 
 if __name__ == '__main__':
